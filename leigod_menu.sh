ac49528231185e1f8607d9fe8cca3a95b3002141
喵二 <66155308+miaoermua@users.noreply.github.com>
2024-07-22 19:52:05 +0800

    Update leigod_menu.sh

--- leigod_menu.sh
+++ leigod_menu.sh
@@ -7,12 +7,16 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 if [ -e /etc/asus_release ]; then
-    echo "TONY 别肘!我是说出台词我爱 BCM!"
+    echo "TONY 别肘!我爱 BCM!"
+    echo ""
+    echo "无法运行 OpenWrt LeigodAcc 管理器"
+    echo "此脚本不适用于 ASUS 请在梅改固件中使用传统方法安装"
     exit 0
 fi
 
 if ! grep -q "OpenWrt" /etc/openwrt_release; then
     echo "Your system is not supported!"
+    echo "别逗，你的系统无法运行 OpenWrt LeigodAcc 管理器!"
     exit 1
 fi
 
@@ -27,10 +31,10 @@ leigod_menu() {
     echo "4. 禁用/启用 雷神服务"
     echo "5. 切换运行模式 (TUN/Tproxy)"
     echo "6. 安装兼容性依赖 (主机优化)"
-    echo "7. 帮助"
+    echo "7. 反馈/帮助"
     echo "0. 退出"
     echo "============================="
-    echo "选择数字功能项并回车执行: "
+    echo -n "选择数字功能项并回车执行: "
 }
 
 install_leigodacc() {
@@ -53,16 +57,14 @@ install_leigodacc() {
 
     if [ -f /etc/catwrt_release ]; then
         if ! grep -q -E "catwrt|repo.miaoer.xyz" /etc/opkg/distfeeds.conf && ! ip a | grep -q -E "192\.168\.[0-9]+\.[0-9]+|10\.[0-9]+\.[0-9]+\.[0-9]+|172\.1[6-9]\.[0-9]+\.[0-9]+|172\.2[0-9]+\.[0-9]+|172\.3[0-1]\.[0-9]+\.[0-9]+"; then
-            echo "[ERROR] 请先配置软件源"
+            echo "[ERROR] 请先配置 CatWrt 软件源"
             echo "Cattools - Apply_repo"
             cattools
             return
         fi
     else
-        echo "/etc/opkg/customfeeds.conf"
-        cat /etc/opkg/customfeeds.conf
-        echo "/etc/opkg/distfeeds.conf"
-        cat /etc/opkg/distfeeds.conf
+        echo "cat /etc/opkg/customfeeds.conf" && cat /etc/opkg/customfeeds.conf
+        echo "cat /etc/opkg/distfeeds.conf" && cat /etc/opkg/distfeeds.conf
         if [ ! -f /usr/bin/cattools ]; then
             echo "[AD] 你还没有安装 Cattools 以方便安装 LeigodAcc 中依赖 Kmod 的组件"
             echo "请查看 https://github.com/miaoermua/cattools 或使用"
@@ -97,12 +99,19 @@ install_leigodacc() {
         fi
     done
 
-    echo "[INFO] 下面是官方脚本输出内容,如遇到问题请截图反馈官方"
+    for pkg in kmod-tun kmod-ipt-tproxy kmod-netem tc-full kmod-ipt-ipset conntrack curl libpcap iptables kmod-ipt-nat iptables-mod-tproxy ipset; do
+        if ! opkg list_installed | grep -q "$pkg"; then
+            echo "[ERROR] 缺少包: $pkg"
+            echo "你可以通过管理器中的安装依赖性组件进行补充!"
+        fi
+    done
+
+    echo "[INFO] 下面是雷神提供的脚本,打印内容偏长如遇到问题请提供输出内容(截图/文字)反馈到群里."
     
     cd /tmp && sh -c "$(curl -fsSL http://119.3.40.126/router_plugin/plugin_install.sh)"
 
     if [ ! -d /usr/sbin/leigod ]; then
-        echo "[ERROR] LeigodAcc 未安装，可能是设备存储已满!"
+        echo "[ERROR] 检测到 LeigodAcc 未安装，有可能是设备存储空间已满!"
         return
     fi
 }
@@ -165,7 +174,7 @@ install_compatibility_dependencies() {
     for pkg in $packages; do
         if ! opkg list_installed | grep -q "$pkg"; then
             echo "[INFO] $pkg 未在官方源中找到，尝试使用第三方源"
-            echo "正在使用天灵 immortalwrt 的软件源，并不是原生支持的软件可能存在官方源除外的问题"
+            echo "正在使用天灵 immortalwrt pku 的软件源，并不是原生支持的软件包可能会存在你所在的第三方固件源除外的问题"
             for url in $urls; do
                 wget -P "$tmp_dir" "$url"
             done
@@ -174,15 +183,22 @@ install_compatibility_dependencies() {
         fi
     done
     rm -rf "$tmp_dir"
+
+    for pkg in kmod-tun kmod-ipt-tproxy kmod-netem tc-full kmod-ipt-ipset conntrack curl libpcap iptables kmod-ipt-nat iptables-mod-tproxy ipset; do
+        if ! opkg list_installed | grep -q "$pkg"; then
+            echo "[ERROR] 缺少包: $pkg"
+            echo "Tip: 你可以到 immoralwrt 官网构建固件并勾选对应的组件,CatWrt 未来将支持 LeigodAcc 依赖."
+        fi
+    done
 }
 
 uninstall_leigodacc() {
     if [ ! -d /usr/sbin/leigod ]; then
-        echo "[ERROR] LeigodAcc 未安装"
+        echo "[ERROR] 雷神服务文件不存在，是不是还没安装捏."
         return
     fi
 
-    echo "确定卸载? 选择数字并回车 10s 后自动卸载 ([1] 确定 / [2] 取消) "
+    echo "确定卸载? 输入数字后回车或 10s 后自动卸载 ([1]确定 / [2]取消): "
     read -t 10 choice
     case $choice in
         1)
@@ -213,7 +229,7 @@ reinstall_leigodacc() {
 
 service() {
     if [ ! -f /etc/init.d/acc ]; then
-        echo "[ERROR] 雷神服务文件不存在"
+        echo "[ERROR] 雷神服务文件不存在，是不是还没安装捏."
         return
     fi
 
@@ -228,7 +244,7 @@ service() {
 
 switch_mode() {
     if [ ! -f /etc/init.d/acc ]; then
-        echo "[ERROR] 雷神服务文件不存在"
+        echo "[ERROR] 雷神服务文件不存在，是不是还没安装捏."
         return
     fi
 
@@ -248,7 +264,11 @@ switch_mode() {
 }
 
 help() {
-    echo "帮助信息："
+    echo ""
+    echo "BUG 反馈请加群: 632342113"
+    echo "Tip: LeigodAcc 特指雷神加速器"
+    echo ""
+    echo "HELP："
     echo "1. 安装：安装 LeigodAcc"
     echo "2. 卸载：卸载 LeigodAcc"
     echo "3. 重装：重装 LeigodAcc"
@@ -256,7 +276,9 @@ help() {
     echo "5. 切换运行模式：在 TUN 和 Tproxy 模式之间切换"
     echo "6. 安装兼容性依赖：尝试使用天灵 immoralwrt pku 源安装常见缺失依赖"
     echo "7. 帮助：显示帮助信息"
-    echo "0. 退出：退出脚本"
+    echo "0. 退出：退出管理器"
+    echo ""
+    sleep 3
 }
 
 # 主程序
@@ -289,7 +311,7 @@ while true; do
             exit 0
             ;;
         *)
-            echo "[ERROR] 无效的选项，请重新输入"
+            echo "[ERROR] 请重新输入对应功能的数字并回车!"
             ;;
     esac
 done
