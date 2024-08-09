34c961cb7f48c2076274ed2795e43575c237ed2b
喵二 <66155308+miaoermua@users.noreply.github.com>
2024-08-09 19:58:17 +0800

    Update leigod.sh

--- leigod.sh
+++ leigod.sh
@@ -21,7 +21,7 @@ if [ -d /userdisk/appdata ]; then
     exit 1
 fi
 
-if ! grep -q -E "OpenWrt|QWRT|ImmortalWrt|iStoreOS" /etc/openwrt_release; then
+if ! grep -qi -E "OpenWrt|QWRT|ImmortalWrt|iStoreOS" /etc/openwrt_release; then
     echo "Your system is not supported!"
     echo "别逗，你的系统无法运行 OpenWrt LeigodAcc 管理器!"
     exit 1
