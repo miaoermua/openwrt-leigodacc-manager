4ee129f4b27897c1f9cb7b6a512a5e8bb6cc59ce
喵二 <66155308+miaoermua@users.noreply.github.com>
2025-06-18 20:59:25 +0800

    Update logs.sh

--- luci-logs/logs.sh
+++ luci-logs/logs.sh
@@ -23,13 +23,13 @@ LOGS_HTM_PATH="/usr/lib/lua/luci/view/leigod/logs.htm"
 
 mkdir -p /usr/lib/lua/luci/view/leigod/
 
-wget -O "$ACC_LUA_PATH" "$ACC_LUA_URL"
+wget -q -O "$ACC_LUA_PATH" "$ACC_LUA_URL"
 if [ $? -ne 0 ]; then
     echo "[ERROR] 下载 $ACC_LUA_URL 失败，请检查网络连接"
     exit 1
 fi
 
-wget -O "$LOGS_HTM_PATH" "$LOGS_HTM_URL"
+wget -q -O "$LOGS_HTM_PATH" "$LOGS_HTM_URL"
 if [ $? -ne 0 ]; then
     echo "[ERROR] 下载 $LOGS_HTM_URL 失败，请检查网络连接"
     exit 1
